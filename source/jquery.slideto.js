b248e3d77f2817468b64e0041dffc4ac2340e4c7
Christophe Verbinnen <djpate@gmail.com>
2011-01-01 16:27:34 +0100

    import

---
+++ source/jquery.slideto.js
@@ -0,0 +1,26 @@
+(function($){
+	$.fn.slideto = function(options) {
+
+		var defaults = {  
+			slide_duration: "slow",
+			highlight_duration: 3000,
+			highlight: true,
+			highlight_color: "#FFFF99"
+		};
+		
+		var options = $.extend(defaults, options);  
+
+		return this.each(function() {
+			
+			obj = $(this);
+			
+			$('body').animate({scrollTop: obj.offset().top},options.duration,function(){
+				if(options.highlight){
+					obj.effect("highlight", {'color': options.highlight_color}, options.highlight_duration);
+				}
+			});
+			
+		});
+		
+	};
+})(jQuery);
