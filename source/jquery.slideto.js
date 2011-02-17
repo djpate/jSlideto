2e36d2ec5c7bb6ef4587c5cc1a65d1a2d3a7922c
Christophe Verbinnen <djpate@gmail.com>
2011-02-17 09:52:44 +0100

    new version

--- source/jquery.slideto.js
+++ source/jquery.slideto.js
@@ -32,7 +32,7 @@
 			
 			obj = $(this);
 			
-			$('body').animate({scrollTop: obj.offset().top},options.duration,function(){
+			$('html').animate({scrollTop: obj.offset().top},options.duration,function(){
 				if(options.highlight  && $.ui.version){
 					obj.effect("highlight", {'color': options.highlight_color}, options.highlight_duration);
 				}
